79db5a038f3868e9e0fddc2de3b8eaa32fea0f38
Germancho01 <germandominguez228@gmail.com>
2022-05-16 20:54:03 -0300

    Update Avion.java, Barco.java, Vehiculo.java

--- src/classes/Avion.java
+++ src/classes/Avion.java
@@ -1,5 +1,30 @@
 package classes;
 
-public class Avion {
+public class Avion extends Vehiculo {
+
+	public Double longitud;
+	public Integer cantPasajeros;
+
+	public Avion(Integer idVehiculo, String nombre, String color, Double longitud, Integer cantPasajeros) {
+		super(idVehiculo, nombre, color);
+		this.longitud = longitud;
+		this.cantPasajeros = cantPasajeros;
+	}
+
+	public Double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(Double longitud) {
+		this.longitud = longitud;
+	}
+
+	public Integer getCantPasajeros() {
+		return cantPasajeros;
+	}
+
+	public void setCantPasajeros(Integer cantPasajeros) {
+		this.cantPasajeros = cantPasajeros;
+	}
 
 }
--- src/classes/Vehiculo.java
+++ src/classes/Vehiculo.java
@@ -1,5 +1,43 @@
 package classes;
 
 public class Vehiculo {
+	public Integer idVehiculo;
+	public String nombre, color;
+
+	public Vehiculo(Integer idVehiculo, String nombre, String color) {
+		super();
+		this.idVehiculo = idVehiculo;
+		this.nombre = nombre;
+		this.color = color;
+	}
+
+	public Integer getIdVehiculo() {
+		return idVehiculo;
+	}
+
+	public void setIdVehiculo(Integer idVehiculo) {
+		this.idVehiculo = idVehiculo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehiculo [idVehiculo=" + idVehiculo + ", nombre=" + nombre + ", color=" + color + "]";
+	}
 
 }
